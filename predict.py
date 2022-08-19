73f8393a5699a4fc78c9255d8384b6121caf4eb6
rowanoaktree <rowan.converse@gmail.com>
2022-08-19 00:12:40 +0000

    plot pr curves

--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@ import rasterio
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
-from utils.prec_rec_curve import precRecCurve
+from utils.prec_rec_curve import precRecCurve, save_precrec_plot
 
 dir = "modelstates"
 model = main.deepforest.load_from_checkpoint("{}/checkpoint.pl".format(dir))
@@ -37,26 +37,23 @@ predictions = model.predict_file(csv_file=get_data(csv_file), root_dir=root_dir)
 areas = (predictions['xmax'] - predictions['xmin']) * (predictions['ymax'] - predictions['ymin'])
 predictions = predictions[areas <= bbox_max_area]
 visualize.plot_prediction_dataframe(predictions, root_dir, None, savedir=os.path.join(save_dir, 'predictions'))
+predictions.to_csv(os.path.join(save_dir, 'predictions.csv'))
 
 #Evaluate
 result = evaluate.evaluate(predictions=predictions, ground_df=pd.read_csv(csv_file), root_dir=root_dir, savedir=os.path.join(save_dir, 'evaluation'))
+result.to_csv(os.path.join(save_dir, 'results.csv'))
 
 # precision-recall curves
-precRec = precRecCurve(result['results'], num_steps=50)
-plt.figure()
-for species in precRec:
-    plt.plot(precRec[species]['recall'], precRec[species]['precision'], title=species)
-    plt.draw()
-plt.title(f'{species}: precision-recall curve')
-plt.xlabel('recall')
-plt.ylabel('precision')
-plt.legend()
-plt.savefig(os.path.join(save_dir, 'prec_rec.png'))
+precRec = precRecCurve(predictions, result['results'], num_steps=50)
+save_precrec_plot(precRec, os.path.join(save_dir, 'prec_rec.png'))
+
+# class-agnostic
+precRec = precRecCurve(predictions, result['results'], num_steps=50, agnostic_label='bird')
+save_precrec_plot(precRec, os.path.join(save_dir, 'prec_rec_classAgnostic.png'))
 
 
+#ap = result["box_precision"].mean()
+#ar = result["box_recall"].mean()
+#perclass = result["class_recall"]
 
-ap = result["box_precision"].mean()
-ar = result["box_recall"].mean()
-perclass = result["class_recall"]
-
-print("The average precision is {}, the average recall is {}".format(ap,ar))
+#print("The average precision is {}, the average recall is {}".format(ap,ar))
--- utils/prec_rec_curve.py
+++ utils/prec_rec_curve.py
@@ -1,21 +1,33 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
-def precRecCurve(df, num_steps=50):
+
+def precRecCurve(df_pred, df_evaluated, num_steps=50, agnostic_label=None):
+    if agnostic_label is not None:
+        df_pred['label'] = agnostic_label
+        df_evaluated['true_label'] = agnostic_label
+        df_evaluated.predicted_label[np.logical_not(pd.isnull(df_evaluated['predicted_label']))] = agnostic_label
+
+    
     # get min-max range of confidence scores
-    minConf = df['score'].min()
-    maxConf = df['score'].max()
+    minConf = df_evaluated['score'].min()
+    maxConf = df_evaluated['score'].max()
     steps = np.linspace(minConf, maxConf, num_steps)
-    labelclasses = df['true_label'].unique()
+    labelclasses = df_evaluated['true_label'].unique()
 
     result = {}
     for labelclass in labelclasses:
         prec, rec = [], []
 
+        num_nan = np.sum((df_evaluated['true_label'] == labelclass) & (np.isnan(df_evaluated['score'])))
+
         for step in steps:
-            rows = df[df['score'] >= step]
+            rows = df_evaluated[df_evaluated['score'] >= step]
             tp = np.sum((rows['predicted_label'] == labelclass) & (rows['true_label'] == labelclass))
-            fp = np.sum((rows['predicted_label'] == labelclass) & (rows['true_label'] != labelclass))
-            fn = np.sum((rows['predicted_label'] != labelclass) & (rows['true_label'] == labelclass))
+            fp = np.sum((rows['predicted_label'] == labelclass) & (rows['true_label'] != labelclass)) + \
+                        np.sum((df_pred.score >= step) & (df_pred.label == labelclass))
+            fn = np.sum((rows['predicted_label'] != labelclass) & (rows['true_label'] == labelclass)) + num_nan
 
             try:
                 p = tp/(tp+fp)
@@ -33,3 +45,35 @@ def precRecCurve(df, num_steps=50):
             'recall': rec
         }
     return result
+
+
+def save_precrec_plot(precRec, destination):
+    plt.figure()
+    for species in precRec:
+        plt.plot(precRec[species]['recall'], precRec[species]['precision'], label=species)
+        plt.draw()
+    plt.title('Precision-recall curve')
+    plt.xlabel('recall')
+    plt.xlim([0, 1])
+    plt.ylabel('precision')
+    plt.ylim([0, 1])
+    plt.grid()
+    plt.legend()
+    plt.savefig(destination)
+
+
+# if __name__ == '__main__':
+#     import os
+#     import pandas as pd
+#     import matplotlib.pyplot as plt
+
+#     save_dir = r"predictions/val"
+#     preds = pd.read_csv('temp.csv')
+#     precRec = precRecCurve(preds)
+#     save_precrec_plot(precRec, os.path.join(save_dir, 'prec_rec.png'))
+
+#     # class-agnostic
+#     preds['true_label'] = 'bird'
+#     preds.predicted_label[np.logical_not(pd.isnull(preds['predicted_label']))] = 'bird'
+#     precRec = precRecCurve(preds)
+#     save_precrec_plot(precRec, os.path.join(save_dir, 'prec_rec_speciesAgnostic.png'))
